82d7ec23e605e03933fb0dd96926cca477be6bb8
Mehran Hydary <mehran.hydary@gmail.comcd>
2015-06-24 19:34:13 -0400

    Fixed first test requirement

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,19 +1,8 @@
 module StringCalculator
-
+    # module means we can call it, we only need one and 
+    # we will call it
   def self.add(string)
-    # code to be tested goes here...
-    # have to convert string into an array of numbers
-    # after that check the array to see if there are one 
-    # two or no numbers 
-    # if it is no numbers, return 0, if there is one
-    # return one number, if there are two numbers, return
-    # the sum of the two numbers 
-    
-    if string.empty?
-    	return 0
-    elsif string.any?{|n| n.to_s.is_i?}
-    	
-    end	
+  	0
   end
 
   # ...and here when the above becomes too complex.
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,13 +6,9 @@ require 'string_calculator'
 # The top of your testing block
 # More examples of tests can be found here: https://github.com/rspec/rspec-expectations
 RSpec.describe StringCalculator, "#add" do
-
+  # this is where we write tests to see if our code is working
   it "returns 0 for empty string" do
     expect(StringCalculator.add("")).to eql(0)
-  end
-
-  it "returns a single number for a string with a single value" do
-  	expect(StringCalculator.add("")).is_i?
   end
 
   # more tests go here
