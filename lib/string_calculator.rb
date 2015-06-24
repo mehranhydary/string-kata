d14bdd75aa38fa0888b53169b6dea9ea45230012
Mehran Hydary <mehran.hydary@gmail.comcd>
2015-06-24 19:48:13 -0400

    Checks for 0 or 5

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,9 @@ module StringCalculator
     # module means we can call it, we only need one and 
     # we will call it
   def self.add(string)
-  	0
+  	# can use two solutions for single method
+  	# string.to_i # this is more simple and trivial
+  	string == "5" ? 5 : 0 # I like this method; checks string, returns 5 or 0
   end
 
   # ...and here when the above becomes too complex.
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,15 +1,31 @@
 #Advanced developers: Uncomment the following understand more about the RSpec "focus" filter
 #require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-require 'string_calculator'
+require 'string_calculator' # this is the code we are testing
 
 # The top of your testing block
 # More examples of tests can be found here: https://github.com/rspec/rspec-expectations
+# These tests are usually long so the contexts give some information for
+# each type of test 
+
+# You can pass a certain argument to only run tests for some contexts
 RSpec.describe StringCalculator, "#add" do
   # this is where we write tests to see if our code is working
-  it "returns 0 for empty string" do
-    expect(StringCalculator.add("")).to eql(0)
-  end
+  	context "null value" do # gives the coder some context; can be anything (e.g. tests that my manager gave me)
+	  it "returns 0 for empty string" do
+	    expect(StringCalculator.add("")).to eql(0)
+	  end
+	end
 
   # more tests go here
+  	context "single value" do
+	  it "returns 0 for the string '0'" do
+	    expect(StringCalculator.add("0")).to eql(0)
+	  end
+	  it "returns 5 for the string '5'" do
+	    expect(StringCalculator.add("5")).to eql(5)
+	  end
+	  
+  	end
+
 end
